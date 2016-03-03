95be9382cd9ecd17c010e70a97f522cf4ae25b36
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-03-03 10:15:09 -0600

    Add all boilerplating for tests

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 /*
  * @credits: http://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript/4819886#4819886
  */
-var isTouchDevice = process.browser && (
+const isTouchDevice = process.browser && (
 	'ontouchstart' in global || // everyone else
 	'onmsgesturechange' in global //IE10
 	);
 
-exports.default = isTouchDevice;
+export default isTouchDevice;
 
-exports.PointerEvents = typeof PointerEvent !== 'undefined'
+export const PointerEvents = typeof PointerEvent !== 'undefined'
 	? {
 		pointerDown: 'pointerdown',
 		pointerEnter: 'pointerenter',
@@ -37,7 +37,3 @@ exports.PointerEvents = typeof PointerEvent !== 'undefined'
 			pointerOver: 'mouseover',
 			pointerUp: 'mouseup'
 		};
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
