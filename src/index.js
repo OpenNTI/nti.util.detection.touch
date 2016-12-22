93aee58d578fc753525b5b9a1c76b0f08480452b
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-12-22 14:03:21 -0600

    Use maxTouchPoints to identify correctly isTouchDevice for IE/Edge/Chrome/Firefox

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-/*
- * @credits: http://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript/4819886#4819886
- */
+const firstNonNull = (value, key) => value != null ? value : navigator[key];
+const MaxTouchPoints = typeof navigator !== 'undefined' ? (['msMaxTouchPoints', 'maxTouchPoints'].reduce(firstNonNull, null)) : null;
+const hasMaxTouchPoints = MaxTouchPoints != null;
 const isTouchDevice = process.browser && (
 	'ontouchstart' in global || // everyone else
 	'onmsgesturechange' in global //IE10
-	);
+	)
+	&& (!hasMaxTouchPoints || MaxTouchPoints > 0);
 
 export default isTouchDevice;
 
